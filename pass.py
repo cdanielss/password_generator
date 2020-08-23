0d21e603b49543ba327d2decf8748a03a8b8056d
cdanielss <cdanielss1973@outlook.com>
2020-08-22 21:48:52 -0300

    Criando campo de caracteres

--- pass.py
+++ pass.py
@@ -4,22 +4,24 @@ import os
 
 class PassGen:
     def __init__(self):
-        sg.theme('Black') # Escolhendo o tema, https://www.geeksforgeeks.org/themes-in-pysimplegui/ 
+        sg.theme('BlueMono') # Escolhendo o tema, https://www.geeksforgeeks.org/themes-in-pysimplegui/ 
         
         # Criando os elementos da pagina
         layout = [
-            [sg.Text('Site/Software:', size=(10,1)), 
+            [sg.Text('Site/Software:', size=(15,1)), 
             sg.Input(key='site', size=(20,1))],
-            [sg.Text('Email/Usuario:', size=(10,1)), 
+            [sg.Text('Email/Usuario:', size=(15,1)), 
             sg.Input(key='usuario', size=(20,1))],
             [sg.Text('Quantidade de Caracteres:'), sg.Combo(values=list(range(30)), key='total_chars',
             default_value=7, size=(3,1) )],
+            [sg.Text('Caracteres Especiais:'), sg.Combo(values=('sim', 'não'), key='esp_chars',
+            default_value='sim', size=(5,1) )],
             [sg.Output(size=(32, 5))],
             [sg.Button('Gerar Senha'), sg.Button('Salvar')]
         ]
 
         # Exibidando a tela
-        self.janela = sg.Window('Password Generator', layout, size=(300,300), grab_anywhere=True)
+        self.janela = sg.Window('Password Generator', layout, grab_anywhere=True, size=(350,250))
 
     def Iniciar(self):
         # Roda a aplicacao
@@ -28,8 +30,12 @@ class PassGen:
             if evento == sg.WINDOW_CLOSED: # Para fechar o programa
                 break
             if evento == 'Gerar Senha': # Se clicar no botao de gerar senhas
-                nova_senha = self.gerar_senha(valores)
-                print(nova_senha) 
+                if valores['esp_chars'] == 'sim':
+                    nova_senha = self.gerar_senha(valores)
+                    print(nova_senha) 
+                if valores['esp_chars'] == 'não':
+                    nova_senha = self.gerar_senhaEsp(valores)
+                    print(nova_senha) 
             if evento == 'Salvar': # Se clicar no botao de salvar
                 self.salvar_senha(nova_senha, valores)
 
@@ -38,6 +44,12 @@ class PassGen:
         chars = random.choices(char_list, k=int(valores['total_chars'])) # Gerar uma quantidade de caracteres, sendo o total deles passado por k=int()
         # valores['total_chars'] esta passando o total de caracteres escolhidos na aplicacao
         new_pass = ''.join(chars) # Esta concatendando a string, ou seja tirando os espacos em branco
+        return new_pass
+
+    def gerar_senhaEsp(self, valores):
+        char_list = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm0123456789'
+        chars = random.choices(char_list, k=int(valores['total_chars'])) 
+        new_pass = ''.join(chars)
         return new_pass
 
     def salvar_senha(self, nova_senha, valores):
